902ee6c09c42c28556a6ae2f5a80bc9c6a55ae81
Jacques Thurling <jthurling78@gmail.com>
2024-12-18 20:39:23 +0200

    Added the statistical functions

--- src/Weather.cpp
+++ src/Weather.cpp
@@ -46,3 +46,47 @@ std::string Weather::goToNextTimeFrame(std::string currentTime) {
 
   return getEarliestTime();
 }
+
+double Weather::getHighestTemp(std::vector<WeatherEntry> &currentTimeEntries){
+  double highestPrice = 0.0;
+
+  for (const WeatherEntry& entry : currentTimeEntries) {
+    if (entry.temp > highestPrice) {
+      highestPrice = entry.temp;
+    }
+  }
+
+  return highestPrice;
+}
+
+double Weather::getOpeningTemp(std::vector<WeatherEntry> &previousTimeEntries){
+  double openingTemp = 0.0;
+
+  for (const WeatherEntry& entry : previousTimeEntries) {
+    openingTemp += entry.temp;
+  }
+
+  return openingTemp / previousTimeEntries.size();
+}
+
+double Weather::getClosingTemp(std::vector<WeatherEntry> &currentTimeEntries){
+  double closingTemp = 0.0;
+
+  for (const WeatherEntry& entry : currentTimeEntries) {
+    closingTemp += entry.temp;
+  }
+
+  return closingTemp / currentTimeEntries.size();
+}
+
+double Weather::getLowestTemp(std::vector<WeatherEntry> &currentTimeEntries){
+  double lowestTemp = 0.0;
+
+  for (const WeatherEntry& entry : currentTimeEntries) {
+    if (entry.temp < lowestTemp) {
+      lowestTemp = entry.temp;
+    }
+  }
+
+  return lowestTemp;
+}
