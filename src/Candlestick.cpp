59d365ef0b42e3fdd80d82abd6a6a9cf6e22e140
Jacques Thurling <jthurling78@gmail.com>
2024-12-19 19:55:04 +0200

    Added mapping to the candlestick data

--- src/Candlestick.cpp
+++ src/Candlestick.cpp
@@ -10,29 +10,63 @@ void Candlestick::printCandleStickChart(std::vector<Candlestick> &candlesticks){
   unsigned int height = 25;
   unsigned int width = 90;
 
-  int temp = 40;
+  int temp = 50;
+
+  int highest = 35;
+  int opening = 23;
+  int closing = 5;
+  int lowest = -10;
 
   for (int i = 0; i < height; i++) {
+    temp = mapYCoordFromIndex(i);
     for (int j = 0; j < width; j++) {
       if (j == 0 && i == height - 1) {
         std::cout << "    └";
       } else if (j == 0) {
-        if ((i % 3) - 1 == 0) {
-          if (temp < 0) {
+        if ((i % 3) == 0) {
+          // Prepare the y-axis
+          if(temp < -12) {
             std::cout << temp << " ┤";
-          } else if (temp == 0) {
+          } else if (temp < 10 && temp > -1) {
             std::cout << "  " << temp << " ┤";
+          } else if (temp < -1 && temp > -12) {
+            std::cout << " " << temp << " ┤";
           } else {
             std::cout << " " << temp << " ┤";
           }
-          temp -= 10;
         } else {
           std::cout << "    │";
         }
       } else if (i == height - 1) {
-        std::cout << "─";
+        // Populate the x-axis
+        if (j % 10 == 0) {
+          std::cout << "┬";
+        } else {
+          std::cout << "─";
+        }
+      } else {
+          if (j % 10 == 0) {
+
+            Candlestick candlestick = candlesticks[(j/10) - 1];
+
+            std::cout << candlestick.lowestTemp;
+
+            if (temp < highest && temp > opening) {
+              std::cout << "│";
+            } else if (temp < opening && temp > closing - 3) {
+              std::cout << "█";
+            } else if (temp < closing && temp > lowest) {
+              std::cout << "│";
+            }
+          } else {
+            std::cout << " ";
+          }
       }
     }
     std::cout << std::endl;
   }
+}
+
+double Candlestick::mapYCoordFromIndex(int index){
+  return 40 - ((80 * index)/25);
 }
--- src/Candlestick.h
+++ src/Candlestick.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 class Candlestick {
+  private:
+    static double mapYCoordFromIndex(int index);
   public:
     Candlestick(double openingTemp, double closingTemp, double highestTemp, double lowestTemp);
 
